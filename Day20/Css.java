4bd967ee91a896bec76267a8f624ca0c600dbe97
Samarth yadav <123004545+Samarthkumaryadav@users.noreply.github.com>
2025-08-24 17:21:22 +0530

    Update Css.java

--- Day20/Css.java
+++ Day20/Css.java
@@ -1,6 +1,5 @@
 package SeleniumDemo;
 
-
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.Keys;
@@ -10,50 +9,55 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class CssDemo {
     public static void main(String[] args) throws InterruptedException {
-        
+
         WebDriver driver = new ChromeDriver();
         driver.get("https://demoqa.com/automation-practice-form");
         driver.manage().window().maximize();
         Thread.sleep(3000);
-    	JavascriptExecutor jse= (JavascriptExecutor)driver;
-		jse.executeScript("window.scrollBy(0,400)");
 
-      
+        JavascriptExecutor jse = (JavascriptExecutor) driver;
+        jse.executeScript("window.scrollBy(0,400)");
+
+        // Personal Details
         WebElement fname = driver.findElement(By.cssSelector("input[id='firstName']"));
-        fname.sendKeys("Nasir");
+        fname.sendKeys("Samarth");
 
         WebElement lname = driver.findElement(By.cssSelector("input[id='lastName']"));
-        lname.sendKeys("Guduru");
+        lname.sendKeys("Yadav");
 
         WebElement email = driver.findElement(By.cssSelector("input[id='userEmail']"));
-        email.sendKeys("nasir@gmail.com");
+        email.sendKeys("samyadav1461316@gmail.com");
 
-       driver.findElement(By.cssSelector("label[for='gender-radio-1']")).click();
-     
+        driver.findElement(By.cssSelector("label[for='gender-radio-1']")).click();
+
         WebElement mobileno = driver.findElement(By.cssSelector("input[id='userNumber']"));
-        mobileno.sendKeys("9618214212");
+        mobileno.sendKeys("6268958328");
 
         // Date of Birth
         driver.findElement(By.cssSelector("input[id='dateOfBirthInput']")).click();
-        driver.findElement(By.cssSelector("select.react-datepicker__year-select > option[value='2025']")).click();
-        driver.findElement(By.cssSelector("select.react-datepicker__month-select > option[value='8']")).click();
-        driver.findElement(By.cssSelector("div[aria-label='Choose Thursday, September 25th, 2025']")).click();
+        driver.findElement(By.cssSelector("select.react-datepicker__year-select > option[value='2001']")).click();
+        driver.findElement(By.cssSelector("select.react-datepicker__month-select > option[value='7']")).click(); // August (0-based index)
+        driver.findElement(By.cssSelector("div[aria-label='Choose Tuesday, August 21st, 2001']")).click();
 
         // Subjects
-        String[] subjects = {"Math", "Physics", "Economics", "English"};
+        String[] subjects = {"Computer Science", "Maths", "English"};
         WebElement sub = driver.findElement(By.cssSelector("input[id='subjectsInput']"));
         for (String subject : subjects) {
             sub.sendKeys(subject);
             sub.sendKeys(Keys.ENTER);
         }
 
+        // Hobbies
+        driver.findElement(By.cssSelector("label[for='hobbies-checkbox-1']")).click();
         driver.findElement(By.cssSelector("label[for='hobbies-checkbox-2']")).click();
-        driver.findElement(By.cssSelector("label[for='hobbies-checkbox-3']")).click();
-    	jse.executeScript("window.scrollBy(0,400)");
- 
-        WebElement img = driver.findElement(By.cssSelector("input[id='uploadPicture']"));
-        img.sendKeys("/Users/sai/Desktop/Wallpapers/DBZ.jpg");
 
+        jse.executeScript("window.scrollBy(0,400)");
+
+        // Upload Picture (change path according to your system)
+        WebElement img = driver.findElement(By.cssSelector("input[id='uploadPicture']"));
+        img.sendKeys("C:/Users/Samarth/Desktop/photo.jpg");
+
+        // State & City
         WebElement state = driver.findElement(By.cssSelector("input[id='react-select-3-input']"));
         state.sendKeys("NCR");
         state.sendKeys(Keys.ENTER);
@@ -62,8 +66,10 @@ public class CssDemo {
         city.sendKeys("Delhi");
         city.sendKeys(Keys.ENTER);
 
+        // Submit Form
         driver.findElement(By.cssSelector("button[id='submit']")).click();
-	    Thread.sleep(2000);
-      
+        Thread.sleep(2000);
+
+        driver.quit();
     }
 }
